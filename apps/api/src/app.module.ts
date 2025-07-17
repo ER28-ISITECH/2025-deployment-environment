93579718b8c42c56deac4a6e18960b3692266bb9
ER28 <l.guerra-rago@reysin.fr>
2025-07-17 11:20:40 +0200

    add basic package and config for nestjs

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {ConfigModule, ConfigService} from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync(
+      {
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+          uri: configService.get<string>('MONGODB_URI'),
+        })
+      }
+    ),
+
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {ValidationPipe} from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.setGlobalPrefix('api');
+  app.enableCors();
+
+
+  const config = new DocumentBuilder()
+    .setTitle('Todo app API')
+    .setDescription('The API documentation for the Todo app')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
